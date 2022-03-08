24e6bf931e588fb7c8a2139a66ff92799e7e3aa0
TonyHuang <1578501542@qq.com>
2022-03-08 21:56:56 +0800

    3/8
    导入红酒数据集，学习 csv 格式文件的读取

--- OpeningPreparation/src/test/java/test.java
+++ OpeningPreparation/src/test/java/test.java
@@ -2,8 +2,12 @@ import org.junit.Test;
 
 public class test {
     @Test
-    public void test(){
+    public void test1(){
         SecureDataAggregationAlgorithm algorithm = new SecureDataAggregationAlgorithm(512);
         algorithm.total();
     }
+
+    @Test
+    public void csvTest(){
+        }
 }
