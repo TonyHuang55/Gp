ae9b408e040bf50580b42641379f49adb511dd9c
TonyHuang <1578501542@qq.com>
2022-03-09 21:59:47 +0800

    3/9
    成功读取 csv 格式的数据（测试）;读取工具类设计

--- OpeningPreparation/src/test/java/test.java
+++ OpeningPreparation/src/test/java/test.java
@@ -1,13 +1,59 @@
+import org.apache.commons.csv.*;
 import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 public class test {
     @Test
-    public void test1(){
+    public void test1() {
         SecureDataAggregationAlgorithm algorithm = new SecureDataAggregationAlgorithm(512);
         algorithm.total();
     }
 
     @Test
-    public void csvTest(){
+    public void csvTest() throws Exception {
+//        String[] headers = new String[]{
+//                "fixed acidity",
+//                "volatile acidity",
+//                "citric acid",
+//                "residual sugar",
+//                "chlorides",
+//                "free sulfur dioxide",
+//                "total sulfur dioxide",
+//                "density",
+//                "pH",
+//                "sulphates",
+//                "alcohol",
+//                "quality"};
+//        CSVFormat format = CSVFormat.EXCEL.withHeader(headers).withDelimiter(';');
+//
+        // 1.配置数据路径，读取数据文件
+        String path = "src/main/resources/database/winequality-red.csv";
+        FileReader fileReader = new FileReader(path);
+        // 2. 读取 csv 文件第一行标题
+        String combineTitle = new BufferedReader(new FileReader(path)).readLine();
+        String[] headers = combineTitle.split(";");
+        for (int i = 0; i < headers.length; i++) {
+            String tmp = headers[i];
+            headers[i] = tmp.substring(1, tmp.length() - 1);
         }
+        System.out.println(Arrays.toString(headers));
+
+        CSVFormat format = CSVFormat.EXCEL.withHeader(headers).withDelimiter(';');
+        // 3. 读取数据
+        CSVParser parser = new CSVParser(fileReader, format);
+        List<CSVRecord> records = parser.getRecords();
+
+        parser.close();
+        fileReader.close();
+
+        int size = records.size();
+        for (int i = 1; i < size; i++) {
+            System.out.println(records.get(i));
+        }
+    }
 }
