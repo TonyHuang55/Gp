5443ca8871a4087dcde3fd144942132f3af6bb02
TonyHuang <1578501542@qq.com>
2022-04-01 22:53:49 +0800

    4/1
    学习线性回归评价指标

--- OpeningPreparation/src/test/java/LinearRegression.java
+++ OpeningPreparation/src/test/java/LinearRegression.java
@@ -249,9 +249,9 @@ public class LinearRegression {
 
     public static void LR(double[][] X, double[] Y) {
         // α ：学习率
-        BigDecimal learning_rate = new BigDecimal("0.0002");
+        BigDecimal learning_rate = new BigDecimal("0.0005");
         // a为常数项，a0 ~ a10 是因子的系数项
-        // 初始值全都为 1
+        // 权重初始值全都为 1
         BigDecimal a = new BigDecimal("1"),
                 a0 = new BigDecimal("1"),
                 a1 = new BigDecimal("1"),
@@ -266,15 +266,12 @@ public class LinearRegression {
                 a10 = new BigDecimal("1");
 
         // 迭代次数
-        int iterator = 500;
+        int iterator = 2000;
         // 用于记录上一轮迭代的损失函数
         BigDecimal lastLoss = new BigDecimal(Double.MAX_VALUE);
         // 用于记录损失函数的最小值，作为终止条件
         BigDecimal minLoss = new BigDecimal(Double.MAX_VALUE);
-        // 用于记录两轮损失函数的差值
-        BigDecimal minDerta = new BigDecimal(Double.MAX_VALUE);
         for (int i = 0; i < iterator; i++) {
-            // 每一轮各个参数的损失函数记录
             BigDecimal gradient_a = new BigDecimal("0"),
                     gradient_a0 = new BigDecimal("0"),
                     gradient_a1 = new BigDecimal("0"),
@@ -288,83 +285,55 @@ public class LinearRegression {
                     gradient_a9 = new BigDecimal("0"),
                     gradient_a10 = new BigDecimal("0");
 
-            BigDecimal sumLoss = new BigDecimal("0"), sumLoss0 = new BigDecimal("0"),
-                    sumLoss1 = new BigDecimal("0"), sumLoss2 = new BigDecimal("0"),
-                    sumLoss3 = new BigDecimal("0"), sumLoss4 = new BigDecimal("0"),
-                    sumLoss5 = new BigDecimal("0"), sumLoss6 = new BigDecimal("0"),
-                    sumLoss7 = new BigDecimal("0"), sumLoss8 = new BigDecimal("0"),
-                    sumLoss9 = new BigDecimal("0"), sumLoss10 = new BigDecimal("0");
             BigDecimal totalLoss = new BigDecimal("0");
             for (int k = 0; k < X.length; k++) {
-                BigDecimal x0 = BigDecimal.valueOf(X[k][0]);
-                BigDecimal x1 = BigDecimal.valueOf(X[k][1]);
-                BigDecimal x2 = BigDecimal.valueOf(X[k][2]);
-                BigDecimal x3 = BigDecimal.valueOf(X[k][3]);
-                BigDecimal x4 = BigDecimal.valueOf(X[k][4]);
-                BigDecimal x5 = BigDecimal.valueOf(X[k][5]);
-                BigDecimal x6 = BigDecimal.valueOf(X[k][6]);
-                BigDecimal x7 = BigDecimal.valueOf(X[k][7]);
-                BigDecimal x8 = BigDecimal.valueOf(X[k][8]);
-                BigDecimal x9 = BigDecimal.valueOf(X[k][9]);
-                BigDecimal x10 = BigDecimal.valueOf(X[k][10]);
-                BigDecimal y = BigDecimal.valueOf(Y[k]);
+                BigDecimal x0 = BigDecimal.valueOf(X[k][0]),
+                        x1 = BigDecimal.valueOf(X[k][1]),
+                        x2 = BigDecimal.valueOf(X[k][2]),
+                        x3 = BigDecimal.valueOf(X[k][3]),
+                        x4 = BigDecimal.valueOf(X[k][4]),
+                        x5 = BigDecimal.valueOf(X[k][5]),
+                        x6 = BigDecimal.valueOf(X[k][6]),
+                        x7 = BigDecimal.valueOf(X[k][7]),
+                        x8 = BigDecimal.valueOf(X[k][8]),
+                        x9 = BigDecimal.valueOf(X[k][9]),
+                        x10 = BigDecimal.valueOf(X[k][10]),
+                        y = BigDecimal.valueOf(Y[k]);
 
                 // y - h(x)
                 BigDecimal func = y.subtract(a.add(a0.multiply(x0)).add(a1.multiply(x1)).add(a2.multiply(x2)).add(a3.multiply(x3)).add(a4.multiply(x4)).
                         add(a5.multiply(x5)).add(a6.multiply(x6)).add(a7.multiply(x7)).add(a8.multiply(x8)).add(a9.multiply(x9)).add(a10.multiply(x10)));
-                BigDecimal func0 = func.multiply(x0);
-                BigDecimal func1 = func.multiply(x1);
-                BigDecimal func2 = func.multiply(x2);
-                BigDecimal func3 = func.multiply(x3);
-                BigDecimal func4 = func.multiply(x4);
-                BigDecimal func5 = func.multiply(x5);
-                BigDecimal func6 = func.multiply(x6);
-                BigDecimal func7 = func.multiply(x7);
-                BigDecimal func8 = func.multiply(x8);
-                BigDecimal func9 = func.multiply(x9);
-                BigDecimal func10 = func.multiply(x10);
-                // ∑(y - h(x))
-                sumLoss = sumLoss.add(func);
-                sumLoss0 = sumLoss0.add(func0);
-                sumLoss1 = sumLoss1.add(func1);
-                sumLoss2 = sumLoss2.add(func2);
-                sumLoss3 = sumLoss3.add(func3);
-                sumLoss4 = sumLoss4.add(func4);
-                sumLoss5 = sumLoss5.add(func5);
-                sumLoss6 = sumLoss6.add(func6);
-                sumLoss7 = sumLoss7.add(func7);
-                sumLoss8 = sumLoss8.add(func8);
-                sumLoss9 = sumLoss9.add(func9);
-                sumLoss10 = sumLoss10.add(func10);
+                BigDecimal size = new BigDecimal(String.valueOf(1.0 / X.length));
                 // ∑(y - h(x))^2
                 totalLoss = totalLoss.add(func.multiply(func));
+                // ∑(y - h(x))
+                gradient_a = gradient_a.subtract(size.multiply(func).multiply(BigDecimal.ONE));
+                gradient_a0 = gradient_a0.subtract(size.multiply(func).multiply(x0));
+                gradient_a1 = gradient_a1.subtract(size.multiply(func).multiply(x1));
+                gradient_a2 = gradient_a2.subtract(size.multiply(func).multiply(x2));
+                gradient_a3 = gradient_a3.subtract(size.multiply(func).multiply(x3));
+                gradient_a4 = gradient_a4.subtract(size.multiply(func).multiply(x4));
+                gradient_a5 = gradient_a5.subtract(size.multiply(func).multiply(x5));
+                gradient_a6 = gradient_a6.subtract(size.multiply(func).multiply(x6));
+                gradient_a7 = gradient_a7.subtract(size.multiply(func).multiply(x7));
+                gradient_a8 = gradient_a8.subtract(size.multiply(func).multiply(x8));
+                gradient_a9 = gradient_a9.subtract(size.multiply(func).multiply(x9));
+                gradient_a10 = gradient_a10.subtract(size.multiply(func).multiply(x10));
 
-                gradient_a = gradient_a.subtract(sumLoss);
-                gradient_a0 = gradient_a0.subtract(sumLoss1);
-                gradient_a1 = gradient_a1.subtract(sumLoss2);
-                gradient_a2 = gradient_a2.subtract(sumLoss3);
-                gradient_a3 = gradient_a3.subtract(sumLoss4);
-                gradient_a4 = gradient_a4.subtract(sumLoss5);
-                gradient_a5 = gradient_a5.subtract(sumLoss6);
-                gradient_a6 = gradient_a6.subtract(sumLoss7);
-                gradient_a7 = gradient_a7.subtract(sumLoss8);
-                gradient_a8 = gradient_a8.subtract(sumLoss9);
-                gradient_a9 = gradient_a9.subtract(sumLoss);
-                gradient_a10 = gradient_a10.subtract(sumLoss);
             }
-            BigDecimal size = new BigDecimal(String.valueOf(1.0 / X.length));
-            a = a.subtract(learning_rate).multiply(gradient_a).multiply(size);
-            a0 = a0.subtract(learning_rate).multiply(gradient_a0).multiply(size);
-            a1 = a1.subtract(learning_rate).multiply(gradient_a1).multiply(size);
-            a2 = a2.subtract(learning_rate).multiply(gradient_a2).multiply(size);
-            a3 = a3.subtract(learning_rate).multiply(gradient_a3).multiply(size);
-            a4 = a4.subtract(learning_rate).multiply(gradient_a4).multiply(size);
-            a5 = a5.subtract(learning_rate).multiply(gradient_a5).multiply(size);
-            a6 = a6.subtract(learning_rate).multiply(gradient_a6).multiply(size);
-            a7 = a7.subtract(learning_rate).multiply(gradient_a7).multiply(size);
-            a8 = a8.subtract(learning_rate).multiply(gradient_a8).multiply(size);
-            a9 = a9.subtract(learning_rate).multiply(gradient_a9).multiply(size);
-            a10 = a10.subtract(learning_rate).multiply(gradient_a10).multiply(size);
+
+            a = a.subtract(learning_rate.multiply(gradient_a));
+            a0 = a0.subtract(learning_rate.multiply(gradient_a0));
+            a1 = a1.subtract(learning_rate.multiply(gradient_a1));
+            a2 = a2.subtract(learning_rate.multiply(gradient_a2));
+            a3 = a3.subtract(learning_rate.multiply(gradient_a3));
+            a4 = a4.subtract(learning_rate.multiply(gradient_a4));
+            a5 = a5.subtract(learning_rate.multiply(gradient_a5));
+            a6 = a6.subtract(learning_rate.multiply(gradient_a6));
+            a7 = a7.subtract(learning_rate.multiply(gradient_a7));
+            a8 = a8.subtract(learning_rate.multiply(gradient_a8));
+            a9 = a9.subtract(learning_rate.multiply(gradient_a9));
+            a10 = a10.subtract(learning_rate.multiply(gradient_a10));
             // loss(θ) = 1/2n * ∑(y - h(x))^2
             BigDecimal lossFunc = totalLoss.divide(new BigDecimal(2 * X.length), 4);
             BigDecimal derta = lastLoss.subtract(lossFunc);
@@ -372,15 +341,17 @@ public class LinearRegression {
             if (lossFunc.compareTo(minLoss) < 0) {
                 minLoss = lossFunc;
             }
-            if ((derta.abs()).compareTo(minDerta) <= 0) {
-                minDerta = derta.abs();
-            }
             System.out.println("第" + (i + 1) + "轮：" + lossFunc);
-            if (lossFunc.compareTo(minLoss) > 0 && lossFunc.compareTo(BigDecimal.ONE) < 0) {
+            if (derta.abs().compareTo(new BigDecimal("0.0001")) < 0) {
                 break;
             } else {
                 lastLoss = lossFunc;
             }
+//            if (lossFunc.compareTo(minLoss) > 0 && lossFunc.compareTo(BigDecimal.ONE) < 0) {
+//                break;
+//            } else {
+//                lastLoss = lossFunc;
+//            }
         }
 
         System.out.println("====================================================");
